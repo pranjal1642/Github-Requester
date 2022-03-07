7360f2c42da44d3f397bf7f8d16dd294dc157b57
@pranjal1642 <pranjalgupta1642@gmail.com>
2022-03-07 15:17:40 +0530

    commit

---
+++ A.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+ const A = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+export default A;
---
+++ src/Config/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default  {
+    apiKey: "AIzaSyCwUkSU1FkUk-qlhKfjnkS0GWbHVb1QAUw",
+    authDomain: "fire-github001.firebaseapp.com",
+    projectId: "fire-github001",
+    storageBucket: "fire-github001.appspot.com",
+    messagingSenderId: "724571803085",
+    appId: "1:724571803085:web:916aaf34b2c8390b604884",
+    measurementId: "G-RDN788PMJW"
+  };
